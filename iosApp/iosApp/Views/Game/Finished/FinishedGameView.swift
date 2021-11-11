ba71de4448ed17e067396ea3de7031e52674251e
Alexey Agapov <agapov.one@gmail.com>
2021-11-11 14:05:01 +0500

    Remove gradient from finished game

--- iosApp/iosApp/Views/Game/Finished/FinishedGameView.swift
+++ iosApp/iosApp/Views/Game/Finished/FinishedGameView.swift
@@ -6,45 +6,28 @@ struct FinishedGameView: View {
   var wrapper: MainScreenStoreWrapper
 
   @State var animated = false
-  @State var animateGradient = false
 
   let state: MainGameState.Finished
 
   var body: some View {
-    ZStack {
-      LinearGradient(
-        colors: [.init(white: 0.5), .init(white: 1)],
-        startPoint: animateGradient ? .topLeading : .topTrailing,
-        endPoint: animateGradient ? .bottomTrailing : .bottomLeading
-      )
-        .ignoresSafeArea()
-        .onAppear {
-          withAnimation(
-            .linear(duration: 2.0)
-              .repeatForever(autoreverses: true)
-          ) {
-            animateGradient.toggle()
-          }
-        }
-      VStack {
-        Text(state.winnerTeam == .blue ? "Blue wins!" : "Red wins!")
-          .foregroundColor(color(for: state.winnerTeam))
-        Button("Start new game", action: { wrapper.store.onRestartGameClicked() })
-          .buttonStyle(.borderedProminent)
-        Button("Revenge", action: { wrapper.store.onRevengeClicked() })
-          .buttonStyle(.bordered)
-          .tint(color(for: state.winnerTeam))
-        NavigationLink(destination: HighScoresView()) {
-          Text("High scores")
-        }
+    VStack {
+      Text(state.winnerTeam == .blue ? "Blue wins!" : "Red wins!")
+        .foregroundColor(color(for: state.winnerTeam))
+      Button("Start new game", action: { wrapper.store.onRestartGameClicked() })
+        .buttonStyle(.borderedProminent)
+      Button("Revenge", action: { wrapper.store.onRevengeClicked() })
         .buttonStyle(.bordered)
-      }.offset(y: animated ? 0 : 300)
-        .onAppear {
-          withAnimation(.easeOut(duration: 0.35)) {
-            animated = true
-          }
+        .tint(color(for: state.winnerTeam))
+      NavigationLink(destination: HighScoresView()) {
+        Text("High scores")
+      }
+      .buttonStyle(.bordered)
+    }.offset(y: animated ? 0 : 300)
+      .onAppear {
+        withAnimation(.easeOut(duration: 0.35)) {
+          animated = true
         }
-    }
+      }
   }
 
   private func color(for team: MainTeam) -> Color {
